19b03007fbef53307c49b748b62ae433d7d05442
James Meekhoff <145402708+jamesMEEKHOFF@users.noreply.github.com>
2023-10-02 18:54:18 -0400

    added grammer

--- src/package_1/helloWorld.java
+++ src/package_1/helloWorld.java
@@ -3,6 +3,6 @@ package package_1;
 public class helloWorld {
    public static void main (String []args) {
      
-     System.out.println("Hello World");
+     System.out.println("Hello World!");
    }
 }
